77c889aa4f186d372010070e1dbc979cf3968418
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-01-11 13:47:08 -0300

    destructuring arrays

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const arr = [4,5];
-
-var [a,b,c] = arr;
-console.log(a,b,c); // Aqui, a variável c retornaria undefined
-
-var [a=1,b=1,c=1] = arr;
-console.log(a,b,c); // Já aqui, a variável c retorna 1
-
-
+for (valorInicial; condicao; incremento) {
+  // bloco de looping
+}
